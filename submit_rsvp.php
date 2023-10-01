13ad6a9e57cab5d12d9d4d1ee6b33fb0356a0aab
Noah <support@fftrealm.com>
2023-10-01 19:42:11 -0400

    Update submit_rsvp.php

--- submit_rsvp.php
+++ submit_rsvp.php
@@ -3,11 +3,12 @@
 
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
     $class = filter_input(INPUT_POST, 'class', FILTER_SANITIZE_STRING);
-    $dinner = filter_input(INPUT_POST, 'dinner', FILTER_SANITIZE_NUMBER_INT);
+    $flight = filter_input(INPUT_POST, 'flight', FILTER_SANITIZE_NUMBER_INT);
+    $food = filter_input(INPUT_POST, 'food', FILTER_SANITIZE_STRING);
 
-    if ($name && $class && $dinner !== false) {
-        $query = "INSERT INTO {$table_prefix}rsvp (name, class, dinner) VALUES (?, ?, ?)";
-        $params = [$name, $class, $dinner];
+    if ($name && $class && $flight !== false && in_array($food, ['normal', 'vegan', 'kosher', 'halal'])) {
+        $query = "INSERT INTO {$table_prefix}rsvp (name, class, flight, food) VALUES (?, ?, ?, ?)";
+        $params = [$name, $class, $flight, $food];
         $result = db_query($query, $params);
         if ($result !== false) {
             echo "RSVP submitted successfully.";
