ebff52fc6837d2b0fa5389d7b922a017583858c6
Noah <support@fftrealm.com>
2023-10-01 21:09:10 -0400

    update to react18

--- submit_rsvp.php
+++ submit_rsvp.php
@@ -9,18 +9,22 @@
 
     if ($data) {
         foreach ($data as $guest) {
+            if (!isset($guest['name']) || !isset($guest['ticketType'])) {
+                echo 'Error: Missing data for one or more guests.';
+                continue;  // Skip to the next iteration if any data is missing
+            }
+
             $name = $guest['name'];
             $ticketType = $guest['ticketType'];
-            $specialMeals = $guest['specialMeals'];
-            $interestInFlightSimulator = $guest['interestInFlightSimulator'];
+            $specialMeals = isset($guest['specialMeals']) ? $guest['specialMeals'] : null;
+            $interestInFlightSimulator = isset($guest['interestInFlightSimulator']) ? $guest['interestInFlightSimulator'] : null;
 
             // Assuming you have a table named 'phpbb_rsvp' with columns as mentioned
             $query = "INSERT INTO phpbb_rsvp (name, class, food, flight) VALUES (?, ?, ?, ?)";
             $params = [$name, $ticketType, $specialMeals, $interestInFlightSimulator];
 
             if (!db_update($query, $params)) {
-                echo 'Error: Database update failed.';
-                exit;
+                echo 'Error: Database update failed for guest: ' . $name;
             }
         }
 
