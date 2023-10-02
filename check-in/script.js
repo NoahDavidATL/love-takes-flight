87cb289db87407f814a6bc1bf543e9fe67a49ab1
Noah Kronemeyer <nkronemeyer@roomstogo.com>
2023-10-02 16:09:21 -0400

    update image

--- check-in/script.js
+++ check-in/script.js
@@ -52,171 +52,158 @@ function GuestForm({index, handleGuestChange}) {
     };
 
     return (
-        <div>
-            <img
-                src="https://pbs.twimg.com/media/DhUsMR6X4AAW0Rp?format=jpg"
-                alt="Top Image"
-                style={{
-                maxWidth: '100%',
-                    height: 'auto',
-                    display: 'block',  // Centers the image when width is less than container width
-                    marginLeft: 'auto',
-                    marginRight: 'auto'
-            }}
-            />
-            <div className="guest-box">
-                <Box display="flex" alignItems="center" width="100%" mb={2}>
-                    <TextField
-                        label="Full Name*"
-                        variant="outlined"
-                        style={{flex: 1, marginRight: 8}}
-                        onChange={handleFullNameChange}
-                        error={fullNameError} // Show error if fullNameError is true
-                        helperText={fullNameError ? 'Full Name is required' : ''}
-                    />
-                </Box>
-                <Box display="flex" alignItems="center" mb={2}>
-                    <FormControl variant="outlined" style={{flex: 1, marginRight: 8}}>
-                        <InputLabel>Ticket Type</InputLabel>
-                        <Select
-                            defaultValue="Delta One"
-                            label="Ticket Type"
-                            onChange={e => handleGuestChange(index, 'ticketType', e.target.value)}
-                        >
-                            <MenuItem value="Delta One">Delta One</MenuItem>
-                            <MenuItem value="Comfort Plus">Comfort Plus</MenuItem>
-                            <MenuItem value="Unaccompanied Minors">Unaccompanied Minors</MenuItem>
-                            <MenuItem value="Infant-in-Arms">Infant-in-Arms</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip1')}>
-                        <div>
-                            <Tooltip
-                                PopperProps={{
-                                    disablePortal: true,
-                                }}
-                                onClose={() => handleTooltipClose('tooltip1')}
-                                open={openTooltips.tooltip1}
-                                disableFocusListener
-                                disableHoverListener
-                                disableTouchListener
-                                title={
-                                    <>
-                                        <Typography color="inherit">
-                                            <b>DELTA ONE</b>: Adults 21+ with access to all you can eat and drink
-                                            (including
-                                            unlimited beer, wine, and spirits). These guests will also have access to the
-                                            737 Flight Simulator. Seating will be in Hanger Two.<br/><br/>
-                                            <b>COMFORT PLUS</b>: Young Adults aged 12+ who are not legally allowed to drink
-                                            alcohol. These guests will sit with the adults for dinner (Hanger Two) and will
-                                            have access to all you can eat and drink (soda, etc). These guests will also
-                                            have access to the 737 Flight Simulator with parents permission.<br/><br/>
-                                            <b>UNACCOMPANIED MINORS</b>: Children aged 3 to 11. These children will sit in a
-                                            separate area away from the adults with babysitters (Hanger One). Food
-                                            appropriate for children will be provided for these guests unless otherwise
-                                            noted by their parents.<br/><br/>
-                                            <b>INFANT-IN-ARMS</b>: Children under 3 years old or children who are still in
-                                            diapers. These children will sit with the adults for dinner (Hanger Two). Please
-                                            bring your own food for these guests unless they will eat mac & cheese.
-                                        </Typography>
-                                    </>
-                                }
-                            >
-                                <IconButton onClick={() => handleTooltipOpen('tooltip1')} color="primary">
-                                    <i className="material-icons">info</i>
-                                </IconButton>
-                            </Tooltip>
-                        </div>
-                    </ClickAwayListener>
-                </Box>
-                <Box display="flex" alignItems="center" mb={2}>
-                    <FormControl variant="outlined" style={{flex: 1, marginRight: 8}}>
-                        <InputLabel>Special Meals</InputLabel>
-                        <Select
-                            label="Special Meals"
-                            onChange={e => handleGuestChange(index, 'specialMeals', e.target.value)}
-                        >
-                            <MenuItem value="Vegan">Vegan</MenuItem>
-                            <MenuItem value="Kosher">Kosher</MenuItem>
-                            <MenuItem value="Halal">Halal</MenuItem>
-                            <MenuItem value="Other">Other</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip2')}>
-                        <div>
-                            <Tooltip
-                                PopperProps={{
-                                    disablePortal: true,
-                                }}
-                                onClose={() => handleTooltipClose('tooltip2')}
-                                open={openTooltips.tooltip2}
-                                disableFocusListener
-                                disableHoverListener
-                                disableTouchListener
-                                title={
-                                    <>
-                                        <Typography color="inherit">
-                                            The Delta One buffet will include many food options, including
-                                            a variety of <b>vegetarian</b> and <b>gluten-free</b> dishes.<br/><br/>
-                                            If you request a special meal, our wedding planner
-                                            will reach out to you a few weeks before the event to confirm your dietary
-                                            restrictions.
-                                        </Typography>
-                                    </>
-                                }
-                            >
-                                <IconButton onClick={() => handleTooltipOpen('tooltip2')} color="primary">
-                                    <i className="material-icons">info</i>
-                                </IconButton>
-                            </Tooltip>
-                        </div>
-                    </ClickAwayListener>
-                </Box>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            onChange={e => handleGuestChange(index, 'interestInFlightSimulator', e.target.checked ? 1 : 0)}
-                            color="primary"
-                        />
-                    }
-                    label={
-                        <span>
-                            Interest in Flight Simulator
-                            <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip3')}>
-                                <div style={{display: 'inline-block', marginLeft: 8}}>  {/* Adjust styling as needed */}
-                                    <Tooltip
-                                        PopperProps={{
-                                            disablePortal: true,
-                                        }}
-                                        onClose={() => handleTooltipClose('tooltip3')}
-                                        open={openTooltips.tooltip3}
-                                        disableFocusListener
-                                        disableHoverListener
-                                        disableTouchListener
-                                        title={
-                                            <>
-                                                <Typography color="inherit">
-                                                    The 737 Flight Simulator sessions last approximately 30 minutes. If
-                                                    you've ever
-                                                    dreamt of piloting a real Boeing 737 (with the guidance of a certified
-                                                    flight
-                                                    trainer) express your interest and we'll ensure this one-of-a-kind
-                                                    wedding
-                                                    experience becomes a reality!
-                                                </Typography>
-                                            </>
-                                        }
-                                    >
-                                        <IconButton onClick={() => handleTooltipOpen('tooltip3')} color="primary">
-                                            <i className="material-icons">info</i>
-                                        </IconButton>
-                                    </Tooltip>
-                                </div>
-                            </ClickAwayListener>
-                        </span>
-                    }
+        <div className="guest-box">
+            <Box display="flex" alignItems="center" width="100%" mb={2}>
+                <TextField
+                    label="Full Name*"
+                    variant="outlined"
+                    style={{flex: 1, marginRight: 8}}
+                    onChange={handleFullNameChange}
+                    error={fullNameError} // Show error if fullNameError is true
+                    helperText={fullNameError ? 'Full Name is required' : ''}
                 />
-            </div>
+            </Box>
+            <Box display="flex" alignItems="center" mb={2}>
+                <FormControl variant="outlined" style={{flex: 1, marginRight: 8}}>
+                    <InputLabel>Ticket Type</InputLabel>
+                    <Select
+                        defaultValue="Delta One"
+                        label="Ticket Type"
+                        onChange={e => handleGuestChange(index, 'ticketType', e.target.value)}
+                    >
+                        <MenuItem value="Delta One">Delta One</MenuItem>
+                        <MenuItem value="Comfort Plus">Comfort Plus</MenuItem>
+                        <MenuItem value="Unaccompanied Minors">Unaccompanied Minors</MenuItem>
+                        <MenuItem value="Infant-in-Arms">Infant-in-Arms</MenuItem>
+                    </Select>
+                </FormControl>
+                <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip1')}>
+                    <div>
+                        <Tooltip
+                            PopperProps={{
+                                disablePortal: true,
+                            }}
+                            onClose={() => handleTooltipClose('tooltip1')}
+                            open={openTooltips.tooltip1}
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            title={
+                                <>
+                                    <Typography color="inherit">
+                                        <b>DELTA ONE</b>: Adults 21+ with access to all you can eat and drink
+                                        (including
+                                        unlimited beer, wine, and spirits). These guests will also have access to the
+                                        737 Flight Simulator. Seating will be in Hanger Two.<br/><br/>
+                                        <b>COMFORT PLUS</b>: Young Adults aged 12+ who are not legally allowed to drink
+                                        alcohol. These guests will sit with the adults for dinner (Hanger Two) and will
+                                        have access to all you can eat and drink (soda, etc). These guests will also
+                                        have access to the 737 Flight Simulator with parents permission.<br/><br/>
+                                        <b>UNACCOMPANIED MINORS</b>: Children aged 3 to 11. These children will sit in a
+                                        separate area away from the adults with babysitters (Hanger One). Food
+                                        appropriate for children will be provided for these guests unless otherwise
+                                        noted by their parents.<br/><br/>
+                                        <b>INFANT-IN-ARMS</b>: Children under 3 years old or children who are still in
+                                        diapers. These children will sit with the adults for dinner (Hanger Two). Please
+                                        bring your own food for these guests unless they will eat mac & cheese.
+                                    </Typography>
+                                </>
+                            }
+                        >
+                            <IconButton onClick={() => handleTooltipOpen('tooltip1')} color="primary">
+                                <i className="material-icons">info</i>
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                </ClickAwayListener>
+            </Box>
+            <Box display="flex" alignItems="center" mb={2}>
+                <FormControl variant="outlined" style={{flex: 1, marginRight: 8}}>
+                    <InputLabel>Special Meals</InputLabel>
+                    <Select
+                        label="Special Meals"
+                        onChange={e => handleGuestChange(index, 'specialMeals', e.target.value)}
+                    >
+                        <MenuItem value="Vegan">Vegan</MenuItem>
+                        <MenuItem value="Kosher">Kosher</MenuItem>
+                        <MenuItem value="Halal">Halal</MenuItem>
+                        <MenuItem value="Other">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip2')}>
+                    <div>
+                        <Tooltip
+                            PopperProps={{
+                                disablePortal: true,
+                            }}
+                            onClose={() => handleTooltipClose('tooltip2')}
+                            open={openTooltips.tooltip2}
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            title={
+                                <>
+                                    <Typography color="inherit">
+                                        The Delta One buffet will include many food options, including
+                                        a variety of <b>vegetarian</b> and <b>gluten-free</b> dishes.<br/><br/>
+                                        If you request a special meal, our wedding planner
+                                        will reach out to you a few weeks before the event to confirm your dietary
+                                        restrictions.
+                                    </Typography>
+                                </>
+                            }
+                        >
+                            <IconButton onClick={() => handleTooltipOpen('tooltip2')} color="primary">
+                                <i className="material-icons">info</i>
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                </ClickAwayListener>
+            </Box>
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        onChange={e => handleGuestChange(index, 'interestInFlightSimulator', e.target.checked ? 1 : 0)}
+                        color="primary"
+                    />
+                }
+                label={
+                    <span>
+                        Interest in Flight Simulator
+                        <ClickAwayListener onClickAway={() => handleTooltipClose('tooltip3')}>
+                            <div style={{display: 'inline-block', marginLeft: 8}}>  {/* Adjust styling as needed */}
+                                <Tooltip
+                                    PopperProps={{
+                                        disablePortal: true,
+                                    }}
+                                    onClose={() => handleTooltipClose('tooltip3')}
+                                    open={openTooltips.tooltip3}
+                                    disableFocusListener
+                                    disableHoverListener
+                                    disableTouchListener
+                                    title={
+                                        <>
+                                            <Typography color="inherit">
+                                                The 737 Flight Simulator sessions last approximately 30 minutes. If
+                                                you've ever
+                                                dreamt of piloting a real Boeing 737 (with the guidance of a certified
+                                                flight
+                                                trainer) express your interest and we'll ensure this one-of-a-kind
+                                                wedding
+                                                experience becomes a reality!
+                                            </Typography>
+                                        </>
+                                    }
+                                >
+                                    <IconButton onClick={() => handleTooltipOpen('tooltip3')} color="primary">
+                                        <i className="material-icons">info</i>
+                                    </IconButton>
+                                </Tooltip>
+                            </div>
+                        </ClickAwayListener>
+                    </span>
+                }
+            />
         </div>
     );
 }
