63b04c3f96d6d3f6f8e6e2adea9e885b002b63b5
Noah Kronemeyer <nkronemeyer@roomstogo.com>
2023-10-04 19:21:12 -0400

    Update submit_rsvp.php

--- check-in/submit_rsvp.php
+++ check-in/submit_rsvp.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'db/db_config.php';
+include '../db/db_config.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
