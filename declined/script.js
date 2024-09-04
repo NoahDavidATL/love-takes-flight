a8fa3fe2479a565072aec0db386094adf913cd55
Noah Kronemeyer <nkronemeyer@roomstogo.com>
2024-09-04 14:33:15 -0400

    Update script.js

--- declined/script.js
+++ declined/script.js
@@ -7,13 +7,13 @@ function DeclinedPage() {
     <div style={{ padding: '20px', textAlign: 'center' }}>
         <div className="header-container">
             <div className="header-text">
-                WE'LL MISS YOU
+                CHECK-IN IS CLOSED
             </div>
             <div className="subheader-text">
-                SORRY YOU CAN'T MAKE IT
+                THANKS FOR STOPPING BY
             </div>
         </div>
-        
+
         <Button variant="contained" color="primary" style={{ marginTop: '20px' }} onClick={() => window.location.href = '/'}>
             RETURN TO SITE
         </Button>
